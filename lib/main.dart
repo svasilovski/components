3b9d44ae742b47fff93fa47d9dfa3676e5e27d1b
Samuel Alberto Vasilovski <svasilovski@localhost.localdomain>
2020-07-13 07:29:11 -0300

    Cteate HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:components/src/pages/home_temp.dart';
+import 'package:components/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Components App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
