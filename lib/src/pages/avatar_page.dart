aeeb95840c76da126962c54dc3531a479831c2ed
svasilovski <samuel.vasilovski@gmail.com>
2020-07-16 07:42:23 -0300

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -5,7 +5,30 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('Avatar Page')
+        title: Text('Avatar Page'),
+        actions: <Widget> [
+          Container(
+            margin: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSC_Uo-_fZWh_PiIuE0eRKjrPVdyTvm2KX_pQ&usqp=CAU'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SV'),
+              backgroundColor: Colors.amberAccent,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://live.staticflickr.com/5285/5224416474_2c3d01b346_b.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
